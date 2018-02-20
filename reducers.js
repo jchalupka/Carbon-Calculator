d6f62c57fc4975d7be1c3f996376487634f7250f
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-02-20 00:06:43 -0500

    added TODO

--- reducers.js
+++ reducers.js
@@ -5,13 +5,13 @@ export default reducer = (state, action) => {
     case actionTypes.updateStartLocation:
       state.startLocation = startLocation;
       return state;
-      
+
     case actionTypes.updateEndLocation:
       state.endLocation = endLocation;
       return state;
 
     case actionTypes.updateMap:
-      // not sure what to do here, no real state change
+      // TODO not sure what to do here, no real state change
       // perhaps update the time the map was last updated?
       return state
     
