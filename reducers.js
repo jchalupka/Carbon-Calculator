8edc65a473ff0b13c1b447d0d621aa9c075131c2
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-02-19 23:59:48 -0500

    adding reducers

---
+++ reducers.js
@@ -0,0 +1,21 @@
+import actionTypes from './actionTypes';
+
+export default reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.updateStartLocation:
+      state.startLocation = startLocation;
+      return state;
+      
+    case actionTypes.updateEndLocation:
+      state.endLocation = endLocation;
+      return state;
+
+    case actionTypes.updateMap:
+      // not sure what to do here, no real state change
+      // perhaps update the time the map was last updated?
+      return state
+    
+    default:
+      return state;
+  }
+}
